0d25d10f994fe20cf6a43307fc89056ef95ed98e
2colours <polgar.marton@windowslive.com>
2024-04-02 12:55:19 +0200

    Make test multiplatform

--- test/initial.test.ts
+++ test/initial.test.ts
@@ -3,14 +3,17 @@ import { describe, expect, it, vi } from 'vitest';
 const dotEnvTomlContent = `TEXT="some text here"
 INTEGER=12345`;
 
-vi.mock('fs', () => ({
-    readFileSync(path: any) {
-        switch (path) {
-            case `${process.cwd()}/.env`:
-                return dotEnvTomlContent;
+vi.mock('fs', async () => {
+    const pathModule = await import('path');
+    return {
+        readFileSync(path: any) {
+            switch (path) {
+                case pathModule.resolve(process.cwd(), '.env'):
+                    return dotEnvTomlContent;
+            }
         }
-    }
-}));
+    };
+});
 
 describe('basic', () => {
     it('reads TOML values correctly when importing the config', async () => {
